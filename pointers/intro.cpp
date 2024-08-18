963b2bd718fbc819844eb4c0cae7114a75f94af1
devAmulya <amulyagupta57121@gmail.com>
2024-08-18 19:39:12 +0530

    pointers

--- pointers/intro.cpp
+++ pointers/intro.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-    int num = 5;
+
+    /*int num = 5;
     cout<<num<<endl;
 
     //address of operator -&
@@ -12,6 +13,53 @@ int main(){
     int *ptr = &num;
     cout<<"Address is : "<<ptr<<endl;
     cout<<"Value is : "<<*ptr<<endl;
+
+    double d = 4.3;
+    double *p2 = &d;
+
+    cout<<"Address is : "<<p2<<endl;
+    cout<<"Value is : "<<*p2<<endl;
+    cout<<"size of integer is "<<sizeof(num)<<endl;
+    cout<<"size of pointer is "<<sizeof(p2)<<endl;*/
+
+    /*Bad habbit
+    //pointer to int is created pointing to some garbage address
+    int *p;
+    cout<<*p<<endl;*/
+
+    /*Null pointer
+    int i = 4;
+    int *p = 0;
+    p = &i;
+    cout<<p<<endl;
+    cout<<*p<<endl;*/
+    
+    /*No change without address
+    int num = 5;
+    int a = num;
+    a++;
+    cout<<num<<endl;*/
+
+    /*Change with memory
+    int num = 5;
+    cout<<num<<endl;
+    int *p = &num;
+    (*p)++;
+    cout<<num<<endl;*/
+
+    /*copying a pointer
+    int *q = p;
+    cout<<p<<" - "<<q<<endl;
+    cout<<*p<<" - "<<*q<<endl;*/
+
+    /*pointer addition
+    int i = 3;
+    int *t = &i;
+    cout<<"before t"<<t<<endl;
+    t = t+1;
+    cout<<"after t"<<t<<endl;*/
+
+    
 
 
     return 0;
