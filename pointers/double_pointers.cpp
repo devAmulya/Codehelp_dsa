7d7a49d890cd7c33f0e49ccb6ceb847d44e6d057
devAmulya <amulyagupta57121@gmail.com>
2024-08-18 21:35:50 +0530

    double pointers

--- pointers/double_pointers.cpp
+++ pointers/double_pointers.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main(){
 
-    int value = 5;
+    /*int value = 5;
     int *ptr = &value;
     int **ptr2 = &ptr; //Double Pointer
 
     cout<<"printing ptr : "<<ptr<<endl;
-    cout<<"addres pf ptr : "<<&ptr<<endl;
+    cout<<"addres of ptr : "<<&ptr<<endl;
     cout<<*ptr2<<endl;
+
+    cout << value << endl;
+    cout << *ptr << endl;
+    cout << **ptr2 <<endl;
+
+    cout << &value << endl;
+    cout << ptr << endl;
+    cout << *ptr2 <<endl;*/
 
 }
