01780e93c22c3a19123e21eb54082966715d152d
devAmulya <amulyagupta57121@gmail.com>
2024-08-18 16:17:02 +0530

    pointers

---
+++ pointers/intro.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    cout<<num<<endl;
+
+    //address of operator -&
+
+    cout<<"address of num is : "<<&num<<endl;
+
+    int *ptr = &num;
+    cout<<"Address is : "<<ptr<<endl;
+    cout<<"Value is : "<<*ptr<<endl;
+
+
+    return 0;
+}
