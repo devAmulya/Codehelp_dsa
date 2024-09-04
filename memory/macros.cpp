9ff81deb94a3c2ca2ac6a96a456166acff7ef731
devAmulya <amulyagupta57121@gmail.com>
2024-09-04 10:25:30 +0530

    macros

--- memory/macros.cpp
+++ memory/macros.cpp
@@ -6,8 +6,15 @@ using namespace std;
 int score = 30;
 
 inline int getMax(int&a, int&b){
-    return(a>b) ? a : b;
-    
+    return(a>b) ? a : b; 
+}
+
+void print(int arr[], int n  , int start = 0) {
+
+    for(int i = start; i<n; i++) {
+        cout << arr[i] << endl;
+    }
+
 }
 
 int main(){
@@ -27,7 +34,7 @@ int main(){
     cout<<score;  //global variable value
     */
 
-    //Inline FUnctions
+    /*Inline FUnctions
     int a = 1, b = 2;
     int ans = 0;
     ans = getMax(a,b);
@@ -38,6 +45,17 @@ int main(){
 
     ans = getMax(a,b);
     cout<< ans<<endl;
+    */
+
+    /*default arguments
+    int arr[5] = {1,4,7,8,9};
+    int size = 5;
+
+    print(arr, size);
+    cout << endl;
+    print(arr, size, 2);
+    */
+    
 
     return  0;
 }
