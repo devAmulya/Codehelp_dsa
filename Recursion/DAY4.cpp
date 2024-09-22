b7d17b00752b47ce9ee83dc50c05442b19084ceb
devAmulya <amulyagupta57121@gmail.com>
2024-09-22 15:15:09 +0530

    recursion

--- Recursion/DAY4.cpp
+++ Recursion/DAY4.cpp
@@ -13,6 +13,45 @@ void reverseString(string& s, int i){
     reverseString(s,i+1);
 }
 
+bool palindrome(string s, int i){
+    int n = s.size();
+    if(i >= n/2){
+        return true;
+    }
+    if(s[i] != s[n-i-1]){
+        return false;
+    }
+    bool ans = palindrome(s,i+1);
+    return ans;
+}
+
+int power(int a, int b){
+    if(b == 0){
+        return 1;
+    }
+    if(b == 1){
+        return a;
+    }
+    int ans = power(a,b/2);
+    if(b%2 == 0){
+        return ans*ans;
+    }else{
+        return a*ans*ans;
+    }
+}
+
+void bubble_sort(int arr[], int size){
+    if(size == 0 || size == 1){
+        return;
+    }
+    for(int i = 0; i < size; i++){
+        if(arr[i] > arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    bubble_sort(arr,size-1);
+}
+
 int main(){
 
     /*Reverse String
@@ -21,9 +60,21 @@ int main(){
     cout<<s;
     */
 
-    //Check Palindrome
+    /*Check Palindrome
+    string s = "abca";
+    cout<<palindrome(s,0);
+    */
 
-    
+    /*power
+    cout<<power(2,5);
+    */
+
+    //Bubble sort
+    int arr[] = {2,1,8,3,9,4};
+    bubble_sort(arr,6);
+    for(int i = 0; i < 6; i++){
+        cout<<arr[i];
+    }
 
 
     return 0;
